6d6e57d2bb0b05065be618bcc38d6c0e5a8e3750
Ducdong94 <ducdong94@gmail.com>
2019-05-30 12:36:10 +0700

    First Commit

---
+++ SuggestionService/src/test/java/com/suggestion/service/SuggestionServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.suggestion.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuggestionServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
